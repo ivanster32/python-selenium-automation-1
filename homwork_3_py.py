8e5c2838fe9597e48e82e7da321391e7a108363f
ivanster32 <113743371+ivanster32@users.noreply.github.com>
2023-06-15 13:54:23 -0400

    HW3 CSS Selector

---
+++ homwork_3_py.py
@@ -0,0 +1,40 @@
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+# from time import sleep
+# Part 1
+# 1) input#ap_email.a-input-text.a-span12.auth-autofocus.auth-required-field
+# 2) i.a-icon.a-icon-logo
+# 3) h1.a-spacing-small
+# 4) input#ap_customer_name.a-input-text.a-span12.auth-autofocus.auth-required-field
+# 5)<input type="password" maxlength="1024" id="ap_password" autocomplete="off" placeholder="At least 6 characters" name="password" tabindex="5" class="a-input-text a-span12 auth-required-field auth-require-fields-match auth-require-password-validation">
+# 6)div.a-alert-content
+# 7)input#ap_password_check.a-input-text.a-span12.auth-required-field.auth-require-fields-match
+# 8)input#continue.a-button-input
+# 9)
+# 10
+# 11) a.a-link-emphasis
+#
+#
+#
+#
+#
+#
+#
+# # get the path to the ChromeDriver executable
+# driver_path = ChromeDriverManager().install()
+#
+# # create a new Chrome browser instance
+# service = Service(driver_path)
+# driver = webdriver.Chrome(service=service)
+# driver.maximize_window()
+#
+# # open the url
+# driver.get('https://www.google.com/')
+# driver.find_element(By.ID, 'ap_email').click()
+#
+#
+#
+#
+# driver.quit()
